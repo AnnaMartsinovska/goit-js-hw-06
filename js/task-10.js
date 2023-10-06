7061d823231ffafa572aa21c42182fb151dd2ced
AnnaMartsinovska <annmartsinovska@gmail.com>
2023-10-06 08:41:38 +0300

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,14 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-const refs = {
-  input: document.querySelector('input'),
-  createBtn: document.querySelector('[data-create]'),
-  destroyBtn: document.querySelector('[data-destroy]'),
-  divBox: document.querySelector('#boxes'),
-};
 
+const input = document.querySelector('input[type = number]');
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+const divCollection = document.querySelector('#boxes');
 
-function createBoxes(amount) {
+btnCreate.addEventListener('click', createBoxes)
+btnDestroy.addEventListener('click', destroyBoxes)
 
-};
+function createBoxes() {
+  const amount = Number(input.value);
+  let boxSize = 30;
+  let newDivArray = [];
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement('div');
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    newDivArray.push(box);
+    // divCollection.append(box);
+    boxSize += 10;
+  }
+  // const divs = newDivArray.map(div => div).join('');
+  divCollection.append(...newDivArray);
+}
+
+function destroyBoxes() {
+  divCollection.textContent = '';
+}
